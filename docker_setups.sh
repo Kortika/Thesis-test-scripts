8d460a423d0e828d01b9ae0903061e7f5f6b88fa
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-04-10 20:08:05 +0200

    added stop options

--- docker_setups.sh
+++ docker_setups.sh
@@ -20,15 +20,63 @@ function cleanup() {
     exit 1 
 }
 
-IP_ADDR=$(hostname -I | head -n1 | awk '{print $1}')
+__ScriptVersion="0.0.1"
 
-echo "IP_ADDR=$IP_ADDR" > resources/.custom_env 
+#===  FUNCTION  ================================================================
+#         NAME:  usage
+#  DESCRIPTION:  Display usage information.
+#===============================================================================
+function usage ()
+{
+    echo "Usage :  $0 [options] [--]
+
+    Options:
+    -s|stop       Stop the containers provided by docker-compose.yaml 
+    -h|help       Display this message
+    -v|version    Display script version"
+
+}    # ----------  end of function usage  ----------
+
+#-----------------------------------------------------------------------
+#  Handle command line arguments
+#-----------------------------------------------------------------------
+
+while getopts ":hvs" opt
+do
+  case $opt in
+    s|stop     )
+        divider; 
+        echo "Stopping docker containers..."; 
+        divider; 
+        docker-compose stop; 
+        exit 0;; 
+
+
+    h|help     )  usage; exit 0   ;;
+
+    v|version  )  echo "$0 -- Version $__ScriptVersion"; exit 0   ;;
+
+    \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+
+    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+
+    * )  echo -e "\n  Option does not exist : $OPTARG\n"
+          usage; exit 1   ;;
+
+  esac    # --- end of case ---
+done
+shift $(($OPTIND-1))
+
+
+IP_ADDR=$(hostname -I | head -n1 | awk '{print $1}')
+ENV_FILE="resources/.custom_env" 
+echo "IP_ADDR=$IP_ADDR" > $ENV_FILE
 
 trap cleanup SIGHUP SIGINT SIGTERM
 
 divider 
 echo "Starting up necessary docker containers for Kafka and Flink..." 
 divider 
-docker-compose --env-file .custom_env up -d 
+docker-compose --env-file $ENV_FILE up -d 
 divider 
 
