a98840f65d9502a88cc7d76b517aae2c3c498b85
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-04-11 18:51:21 +0200

    indent and fix pathing issues

--- job_submitter.sh
+++ job_submitter.sh
@@ -26,29 +26,34 @@ function usage ()
 
 while getopts ":hvm:o:" opt
 do
-  case $opt in
-    m|mapping-file ) 
-        MAPPING_FILE_CONTAINER_PATH=$OPTARG ;; 
-    o|output-path ) 
-        OUTPUT_PATH=$OPTARG ;; 
+    case $opt in
+        m|mapping-file ) 
+            MAPPING_FILE_CONTAINER_PATH=$OPTARG ;; 
+        o|output-path ) 
+            OUTPUT_PATH=$OPTARG ;; 
 
-    h|help     )  usage; exit 0   ;;
+        h|help     )  usage; exit 0   ;;
 
-    v|version  )  echo "$0 -- Version $__ScriptVersion"; exit 0   ;;
+        v|version  )  echo "$0 -- Version $__ScriptVersion"; exit 0   ;;
 
-    \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+        \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
 
-    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+        :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
 
-    * )  echo -e "\n  Option does not exist : $OPTARG\n"
-          usage; exit 1   ;;
+        * )  echo -e "\n  Option does not exist : $OPTARG\n"
+              usage; exit 1   ;;
 
-  esac    # --- end of case ---
+    esac    # --- end of case ---
 done
 shift $(($OPTIND-1))
 
 ${MAPPING_FILE_CONTAINER_PATH:?Missing mapping file -m}
 ${OUTPUT_PATH:?Missing output path -o}
+
+
+BASE_DATA_PATH="/mnt/data/"
+MAPPING_FILE_CONTAINER_PATH="${BASE_DATA_PATH}${MAPPING_FILE_CONTAINER_PATH}" 
+OUTPUT_PATH="${BASE_DATA_PATH}${OUTPUT_PATH}"
 
 JOB_CLASS_NAME="io.rml.framework.Main"
 JM_CONTAINER=$(docker ps --filter name=jobmanager --format={{.ID}})
