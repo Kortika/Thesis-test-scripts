bdd6a8b25ee5ee90481e2d9cc0ba91b748785314
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-04-12 02:39:49 +0200

    fixed mapping files and updated metrics scripts

--- metrics.py
+++ metrics.py
@@ -33,6 +33,7 @@ async def calc_metrics(api_url, keys, file):
             metrics_request = requests.get(url=api_url, params=metrics_param)
             metric_response = metrics_request.json()
 
+            
             if count == 0:
                 header = toCSVHeader(metric_response)
                 writer.writerow(header)
@@ -41,12 +42,40 @@ async def calc_metrics(api_url, keys, file):
             writer.writerow(toCSVRow(metric_response))
             await asyncio.sleep(1)
 
-def run_async_loop(url, keys, outputfile): 
-    loop=asyncio.get_event_loop()
-    task =loop.create_task(
-        calc_metrics(url, keys, outputfile))
+def get_vertex_id_name(url, pattern="Window"): 
+    response = requests.get(url=url).json() 
+    print(url) 
 
-    loop.call_later(5, task.cancel)
+    for vertex in response["vertices"]: 
+        if pattern in vertex["name"]: 
+            return (vertex["id"], vertex["name"])
+    return None
+
+
+def get_job_id(url, status="RUNNING"): 
+
+    response = requests.get(url=url).json() 
+    
+    for job in response["jobs"]: 
+        if job["status"] == status: 
+            return job["id"]
+    return None
+
+def get_metrics_id(url, pattern): 
+    response = requests.get(url=url).json() 
+    result = []
+    for met_id in response: 
+        if pattern in met_id["id"]: 
+            result.append(met_id["id"]) 
+    
+    return result
+            
+
+def run_async_loop(aysnc_calls, time_seconds=20): 
+    loop=asyncio.get_event_loop()
+    for cal in async_calls: 
+        task = loop.create_task(cal)
+        loop.call_later(time_seconds, task.cancel)
 
     try:
         loop.run_until_complete(task)
@@ -60,40 +89,35 @@ if __name__ == "__main__":
     parser.add_argument('--interval', type=int, default=5,
                         required=False,
                         help='Interval in seconds to gather the metrics from api. ')
-    parser.add_argument('jobid', type=str,
-                        help='Flink\'s job id')
-    parser.add_argument('vertexid', type=str,
-            help='A vertex of the given job identified in [jobid]')
-
     parser.add_argument('--output', type=str, default="dynamic", required=False,
                 help="Key of the  output file where [output]_subtasks.csv and [output]_mcl.csv will be generated.")
 
     args=parser.parse_args()
+    BASE_URL="http://localhost:8081/v1"
 
-    URL="http://localhost:8081/v1"
-
-
-    subtask_metrics_url=f"{URL}/jobs/{args.jobid}/vertices/{args.vertexid}/subtasks/metrics"
-
-    subtask_metrics_keys=','.join([
-            "numRecordsIn",
-            "numRecordsOut",
-            "numRecordsInPerSecond",
-            "numRecordsOutPerSecond"
-            ])
-
-    run_async_loop(subtask_metrics_url, subtask_metrics_keys, f"{args.output}_subtasks.csv")
-
+    URL=f"{BASE_URL}/jobs"
     
-    jobmanager_metrics_url = f"{URL}/jobmanager/metrics"
+    jobid = get_job_id(URL, "CANCELED") 
+    URL = f"{URL}/{jobid}" 
+    vertexid, vertexname = get_vertex_id_name(URL)
+
+    subtask_metrics_url=f"{URL}/vertices/{vertexid}/subtasks/metrics"
+    
+    subtask_metrics_keys=','.join(get_metrics_id(subtask_metrics_url,"Window"))
+
+    jobmanager_metrics_url = f"{BASE_URL}/jobmanager/metrics"
     jobmanager_metrics_keys = ','.join([
             "Status.JVM.CPU.Load", 
             "Status.JVM.Memory.Heap.Used", 
             "Status.JVM.Memory.Heap.Committed",
             "Status.JVM.Memory.Heap.Max"
             ])
-    
-    run_async_loop(jobmanager_metrics_url, jobmanager_metrics_keys, f"{args.output}_job.csv")
+    async_calls = []
+
+    async_calls.append(calc_metrics(subtask_metrics_url, subtask_metrics_keys, f"{args.output}_subtasks.csv")) 
+    async_calls.append(calc_metrics(jobmanager_metrics_url, jobmanager_metrics_keys, f"{args.output}_job.csv"))
+
+    run_async_loop(async_calls)
 
 
 
