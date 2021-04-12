c1c718b0187fccaf53feaa27a550d312c7c8ef10
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-04-12 16:49:45 +0200

    clear docker volume to ready the next experiment

--- read-from-topic.sh
+++ read-from-topic.sh
@@ -8,4 +8,5 @@ export KAFKA_ADVERTISED_HOST_NAME=$(hostname -I | head -n1 | awk '{print $1;}')
 docker run --rm -it \
 	wurstmeister/kafka:2.12-2.1.1 ./opt/kafka_2.12-2.1.1/bin/kafka-console-consumer.sh \
   --bootstrap-server $KAFKA_ADVERTISED_HOST_NAME:9092 \
-  --topic $TOPIC_NAME
+  --topic $TOPIC_NAME \
+  --from-beginning
---
+++ src/clear_setups.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+ 
+
+
+docker-compose stop 
+docker-compose rm 
+docker volume prune
+
