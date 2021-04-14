3474def95a48888d184982f72d4f9a306097f699
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-04-14 18:01:44 +0200

    fix cleaner and updated code

--- src/output_cleaner.sh
+++ src/output_cleaner.sh
@@ -1,15 +1,55 @@
 #!/usr/bin/env bash
+__ScriptVersion="0.0.1"
+
+#===  FUNCTION  ================================================================
+#         NAME:  usage
+#  DESCRIPTION:  Display usage information.
+#===============================================================================
+function usage ()
+{
+    echo "Usage :  $0 [options] [--] nqDirectory
+
+    Options:
+    -h|help       Display this message
+    -v|version    Display script version"
+
+}    # ----------  end of function usage  ----------
+
+#-----------------------------------------------------------------------
+#  Handle command line arguments
+#-----------------------------------------------------------------------
+
+while getopts ":hv" opt
+do
+  case opt in
+
+    h|help     )  usage; exit 0   ;;
+
+    v|version  )  echo "$0 -- Version opjt"; exit 0   ;;
+
+    * )  echo -e "\n  Option does not exist : OPTARG\n"
+                usage; exit 1   ;;
+
+  esac    # --- end of case ---
+done
+shift $((OPTIND-1))
+
+DIR="$1"
+
+if [[ ! -d "$DIR" ]]; then 
+	echo "Given directory of nq files doesn't exists: $DIR" 
+	exit 1
+fi
 
 
-for file in *.nq ; do
-    cleaned_file="${file}_cleaned"
+CLEANED_DIR="${DIR}cleaned"
 
+mkdir -p "$CLEANED_DIR"
+
+for file in ${DIR}*.nq ; do
+    echo "Cleanin file $file..."
+    cleaned_file="${CLEANED_DIR}/${file##*/}.csv"
     grep -oP "lane[0-9]+.*?timestamp.*?>" $file | paste -d " "  - - | sed -E "s/\?|\&| /,/g" > $cleaned_file 
-    sed -i "s/[<>]//g" $cleaned_file
-    sed -i "s/%20/ /g" $cleaned_file
-    sed -i "s/%3A/ /g" $cleaned_file
-    sed -i "s/-/ /g" $cleaned_file
-    sed -i -E "s/[a-z]+=//g" $cleaned_file
-    awk -F"," '{OFS=","; $6=mktime($6); $13=mktime($13); print $0}'  $cleaned_file > .tmp 
-    mv .tmp $cleaned_file
+    sed -e "s/[<>]//g" -e "s/%20/ /g" -e "s/%3A/ /g" -e "s/-/ /g" -Ee "s/[a-z]+=//g" < $cleaned_file > .tmp 
+    awk -F"," '{OFS=","; $6=mktime($6); $13=mktime($13); print $0}' .tmp > $cleaned_file 
 done
--- src/sort_big_output.sh
+++ src/sort_big_output.sh
@@ -1,9 +1,52 @@
 #!/usr/bin/env bash
 
+__ScriptVersion="0.0.1"
 
-for file in *.nq; 
+#===  FUNCTION  ================================================================
+#         NAME:  usage
+#  DESCRIPTION:  Display usage information.
+#===============================================================================
+function usage ()
+{
+    echo "Usage :  $0 [options] [--] nqDirectoryy
+
+    Options:
+    -h|help       Display this message
+    -v|version    Display script version"
+
+}    # ----------  end of function usage  ----------
+
+#-----------------------------------------------------------------------
+#  Handle command line arguments
+#-----------------------------------------------------------------------
+
+while getopts ":hv" opt
+do
+  case opt in
+
+    h|help     )  usage; exit 0   ;;
+
+    v|version  )  echo "$0 -- Version __ScriptVersion"; exit 0   ;;
+
+    * )  echo -e "\n  Option does not exist : OPTARG\n"
+                usage; exit 1   ;;
+
+  esac    # --- end of case ---
+done
+shift $((OPTIND-1))
+
+DIR=$1
+
+if [[ ! -d "$DIR" ]]; then
+
+    echo "Given directory of benchmark csv files doesn't exists: $DIR" 
+    exit 1
+fi
+
+
+for file in "$DIR/*.csv"; 
 do 
     echo "Sorting $file..."
     sort -o $file $file 
 done
-sort -m *.nq | split -d -1 10000 - output
+sort -m *.csv > final_output.csv  
