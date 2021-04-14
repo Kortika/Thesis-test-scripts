e0ae41e385eeb47b128b270cab291793aaaf2f09
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-04-14 18:33:00 +0200

    parallelize cleaning

--- src/output_cleaner.sh
+++ src/output_cleaner.sh
@@ -41,15 +41,27 @@ if [[ ! -d "$DIR" ]]; then
 	exit 1
 fi
 
+clean_file() {
+    file="$1"
+    local CLEANED_DIR="$2"
+    echo "Cleanin file $file..."
+    cleaned_file="${CLEANED_DIR}/${file##*/}.csv"
+    grep -oP "lane[0-9]+.*?timestamp.*?>" $file | paste -d " "  - - | sed -E "s/\?|\&| /,/g" > $cleaned_file 
+    sed -e "s/[<>]//g" -e "s/%20/ /g" -e "s/%3A/ /g" -e "s/-/ /g" -Ee "s/[a-z]+=//g" < $cleaned_file > "$1.tmp" 
+    awk -F"," '{OFS=","; $6=mktime($6); $13=mktime($13); print $0}' "$1.tmp" > $cleaned_file 
+    rm "$1.tmp"
+}
+
+
 
 CLEANED_DIR="${DIR}cleaned"
 
 mkdir -p "$CLEANED_DIR"
+N=10
 
+(
 for file in ${DIR}*.nq ; do
-    echo "Cleanin file $file..."
-    cleaned_file="${CLEANED_DIR}/${file##*/}.csv"
-    grep -oP "lane[0-9]+.*?timestamp.*?>" $file | paste -d " "  - - | sed -E "s/\?|\&| /,/g" > $cleaned_file 
-    sed -e "s/[<>]//g" -e "s/%20/ /g" -e "s/%3A/ /g" -e "s/-/ /g" -Ee "s/[a-z]+=//g" < $cleaned_file > .tmp 
-    awk -F"," '{OFS=","; $6=mktime($6); $13=mktime($13); print $0}' .tmp > $cleaned_file 
+    ((i=i%N)); ((i++==0)) && wait
+    clean_file $file $CLEANED_DIR &
 done
+)
