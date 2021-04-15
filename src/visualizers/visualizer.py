575d61449490d440e28a48c9e38fb28de09afadb
Sitt Min Oo <sittminoo1995145@gmail.com>
2021-04-15 18:50:18 +0200

    finish visualization script for the majority of the metrics

--- src/visualizers/visualizer.py
+++ src/visualizers/visualizer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import argparse
+from enum import Enum
 from pathlib import Path
 from typing import Any, List
 
+import matplotlib
 from matplotlib.axes import Axes
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,8 +12,16 @@ import pandas as pd
 
 import src.visualizers.error as err
 import src.visualizers.metric_parser as m_parser
-
 plt.style.use("ggplot")
+matplotlib.rcParams['figure.figsize'] = (8, 6)
+
+
+class TestType(Enum):
+    constant = "constant"
+    periodic = "periodic"
+
+    def __str__(self) -> str:
+        return self.value
 
 
 def format_bytes(size):
@@ -65,8 +75,8 @@ def draw_lineplot(used_avg_df: pd.DataFrame,
                   x: List[Any],
                   title: str,
                   xlabel: str,
-                  ylabel: str):
-    ax = plt.subplot(111)
+                  ylabel: str,
+                  ax: Axes):
 
     ax.plot(x, used_avg_df)
     ax.set_ylabel(ylabel)
@@ -78,69 +88,139 @@ def draw_lineplot(used_avg_df: pd.DataFrame,
 
 def visualize_latency(dyn_latency: m_parser.DFConsolidator,
                       tumb_latency: m_parser.DFConsolidator,
-                      output_dir: Path):
+                      output_dir: Path,
+                      test_type: TestType):
 
     metric = "Latency_avg"
     dyn_df = dyn_latency.get_columns(metric=metric)
     tumb_df = tumb_latency.get_columns(metric=metric)
+
+    dyn_df.columns = ["VCTWindow latency (ms)" for _ in dyn_df.columns]
+    tumb_df.columns = ["Tumbling latency (ms)" for _ in tumb_df.columns]
+    # removing all zero values due to starting metric collection late
     dyn_df = dyn_df.loc[(dyn_df != 0).all(axis=1), :]
     tumb_df = tumb_df.loc[(tumb_df != 0).all(axis=1), :]
 
     ax = plt.subplot(111)
     draw_boxplot_ax(dyn_df,
                     [""],
-                    "Data stream with constant rate",
+                    f"Data stream with {test_type} rate",
                     "VCTWindow",
                     "Latencies (ms)",
                     ax=ax)
     plt.savefig(output_dir.joinpath("VCTWindow_latency_boxplot.png"))
-
     plt.close()
 
-    ax = plt.subplot(1, 1, 1)
+    ax = plt.subplot(111)
     draw_boxplot_ax(tumb_df,
                     [""],
-                    "Data stream with constant rate",
+                    f"Data stream with {test_type} rate",
                     "TumblingWindow",
                     "Latencies (ms)",
                     ax=ax)
     plt.savefig(output_dir.joinpath("TumblingWindow_latency_boxplot.png"))
     plt.close()
 
+    # Line latency plotting
+    ax = plt.subplot(111)
+    draw_lineplot(dyn_df,
+                  np.arange(0, dyn_df.shape[0]),
+                  f"Data stream with {test_type} rate",
+                  ylabel="Latency (ms)",
+                  xlabel="Time period (s)",
+                  ax=ax)
+
+    plt.savefig(output_dir.joinpath("VCTWindow_latency_lineplot.png"))
+    plt.close()
+
+    ax = plt.subplot(111)
+    draw_lineplot(tumb_df,
+                  np.arange(0, tumb_df.shape[0]),
+                  f"Data stream with {test_type} rate",
+                  ylabel="Latency (ms)",
+                  xlabel="Time period (s)",
+                  ax=ax)
+
+    plt.savefig(output_dir.joinpath("Tumbling_latency_lineplot.png"))
+    plt.close()
+    pass
+
+
+def visualize_throughput(dyn_vert: m_parser.DFConsolidator,
+                         tumb_vert: m_parser.DFConsolidator,
+                         output_dir: Path,
+                         test_type: TestType):
+
+    dyn_out_avg = dyn_vert.get_columns(metric="RecordsOutPerSecond_avg")
+    tumb_vert_avg = tumb_vert.get_columns(metric="RecordsOutPerSecond_avg")
+
+    throughput_df = pd.concat([dyn_out_avg, tumb_vert_avg], axis=1)
+
+    throughput_df.columns = ["VCTWindow", "TumblingWindow"]
+
+    ax = plt.subplot(111)
+    ax = draw_lineplot(throughput_df,
+                       np.arange(0, dyn_out_avg.shape[0]),
+                       f"Data stream with {test_type} rate",
+                       ylabel="Throughput (records/s)",
+                       xlabel="Time period (s)",
+                       ax=ax)
+
+    plt.savefig(output_dir.joinpath("throughput_comparison.png"))
+    plt.close()
+
     pass
 
 
 def visualize_jvm_stats(dyn_task: m_parser.DFConsolidator,
-                        tumb_task: m_parser.DFConsolidator):
+                        tumb_task: m_parser.DFConsolidator,
+                        output_dir: Path,
+                        test_type: str):
 
-    print(dyn_task._big_frame.columns)
     used_avg_df = dyn_task.get_columns(metric="Used_avg")
-    used_max_df = dyn_task.get_columns(metric="Used_max")
 
-    print(np.arange(0, used_avg_df.shape[0]))
     used_avg_df = used_avg_df.apply(
         lambda row: [format_bytes(x) for x in row])
+    ax = plt.subplot(111)
     ax = draw_lineplot(used_avg_df,
                        np.arange(0, used_avg_df.shape[0]),
-                       "Datastream with constant rate",
+                       f"Datastream with {test_type} rate",
                        ylabel="Memory (MB)",
-                       xlabel="Time period (s)")
+                       xlabel="Time period (s)",
+                       ax=ax)
     plt.savefig(output_dir.joinpath("mem_usage_dynamic.png"))
 
     plt.close()
 
     tumb_used_avg_df = tumb_task.get_columns(metric="Used_avg")
-    tumb_used_max_df = dyn_task.get_columns(metric="Used_max")
 
     tumb_used_avg_df = tumb_used_avg_df.apply(
         lambda row: [format_bytes(x) for x in row])
+    ax = plt.subplot(111)
     ax = draw_lineplot(tumb_used_avg_df,
                        np.arange(0, tumb_used_avg_df.shape[0]),
-                       "Datastream with constant rate",
+                       f"Datastream with {test_type} rate",
                        ylabel="Memory (MB)",
-                       xlabel="Time period (s)")
+                       xlabel="Time period (s)",
+                       ax=ax)
 
     plt.savefig(output_dir.joinpath("mem_usage_tumb.png"))
+    plt.close()
+
+    dyn_cpu_df = dyn_task.get_columns(metric="CPU.Load_avg")
+    tumb_cpu_df = tumb_task.get_columns(metric="CPU.Load_avg")
+
+    cpu_df = pd.concat([dyn_cpu_df, tumb_cpu_df], axis=1)
+    cpu_df.columns = ["VCTWindow", "Tumbling"]
+    ax = plt.subplot(111)
+    ax = draw_lineplot(cpu_df,
+                       np.arange(0, cpu_df.shape[0]),
+                       f"Datastream with {test_type} rate",
+                       ylabel="CPU (percentage)",
+                       xlabel="Time period (s)",
+                       ax=ax)
+
+    plt.savefig(output_dir.joinpath("cpu_comparison.png"))
     plt.close()
 
 
@@ -149,6 +229,11 @@ if __name__ == '__main__':
         description="Script to parse all the gathered csv \
         metrics into pandas dataframes")
 
+    parser.add_argument(
+        "test_type", type=TestType,
+        choices=list(TestType),
+        help="Characteristic of data stream"
+    )
     parser.add_argument(
         "dynamic_metrics_dir", type=str,
         help="Directory containing the csv files \
@@ -178,7 +263,8 @@ if __name__ == '__main__':
     (tumb_latency, tumb_task, tumb_vertex) = m_parser \
         .get_consolidated_dataframes(tumbling_dir)
 
-    visualize_jvm_stats(dyn_task, tumb_task)
-    visualize_latency(dyn_latency, tumb_latency, output_dir)
+    visualize_jvm_stats(dyn_task, tumb_task, output_dir, args.test_type)
+    visualize_latency(dyn_latency, tumb_latency, output_dir, args.test_type)
+    visualize_throughput(dyn_vertex, tumb_vertex, output_dir, args.test_type)
 
     pass
